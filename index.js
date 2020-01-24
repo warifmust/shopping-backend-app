c691fdc2ebbfd62876aba9a31c3e8819074f3726
Arif Mustaffa <wmarif.wm@trinitywizards.com>
2020-01-24 11:11:04 +0800

    arif/add login

--- index.js
+++ index.js
@@ -1,27 +1,35 @@
-const express = require('express');
-const path = require('path');
-const graphqlHTTP = require('express-graphql');
-const mongoose = require('mongoose');
-const rootResolver = require('./server/resolvers/index');
-const graphQlSchema = require('./server/schema/index');
-const generatePassword =  require('password-generator');
+const express = require("express");
+const path = require("path");
+const graphqlHTTP = require("express-graphql");
+const mongoose = require("mongoose");
+const rootResolver = require("./server/resolvers/index");
+const graphQlSchema = require("./server/schema/index");
+const generatePassword = require("password-generator");
+const bodyParser = require("body-parser");
+const isAuth = require("./server/middleware/is-auth");
 
 const app = express();
 
-app.use('/graphql', graphqlHTTP({
-  schema: graphQlSchema,
-  rootValue: rootResolver,
-  graphiql: true,
-}));
+app.use(bodyParser.json());
+app.use(isAuth);
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: graphQlSchema,
+    rootValue: rootResolver,
+    graphiql: true
+  })
+);
 
 // Put all API endpoints under '/api'
-app.get('/api/passwords', (req, res) => {
+app.get("/api/passwords", (req, res) => {
   const count = 5;
 
   // Generate some passwords
   const passwords = Array.from(Array(count).keys()).map(i =>
     generatePassword(12, false)
-  )
+  );
 
   // Return them as json
   res.json(passwords);
@@ -30,22 +38,25 @@ app.get('/api/passwords', (req, res) => {
 });
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
-
+app.use(express.static(path.join(__dirname, "client/build")));
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
 const port = process.env.PORT || 4000;
-mongoose.set( 'useNewUrlParser', true );
-mongoose.set( 'useUnifiedTopology', true );
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useUnifiedTopology", true);
 mongoose
-.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-p8tzi.mongodb.net/${process.env.PROJECT_NAME}?retryWrites=true&w=majority`)
-.then(() => {
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-p8tzi.mongodb.net/${process.env.PROJECT_NAME}?retryWrites=true&w=majority`
+  )
+  .then(() => {
     app.listen(port);
-    console.log(`Running a GraphQL API server at http://localhost:${port}/graphql`);
+    console.log(
+      `Running a GraphQL API server at http://localhost:${port}/graphql`
+    );
   })
-  .catch((err) => console.log(err));
+  .catch(err => console.log(err));
--- server/resolvers/user.js
+++ server/resolvers/user.js
@@ -1,28 +1,60 @@
 const User = require("../models/user");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 module.exports = {
   signUpUser: async args => {
     try {
-      const hashedPassword = await bcrypt.hash(args.userInput.password, 12);
-      const user = new User({
-        name: args.userInput.name,
-        email: args.userInput.email,
-        password: hashedPassword,
-        phoneNumber: args.userInput.phoneNumber
-      });
-      const saveUser = await user.save();
+      const checkUser = await User.findOne({ email: args.userInput.email });
+      if (checkUser) {
+        throw new Error("User exist!");
+      } else {
+        const hashedPassword = await bcrypt.hash(args.userInput.password, 12);
+        const user = new User({
+          name: args.userInput.name,
+          email: args.userInput.email,
+          password: hashedPassword,
+          phoneNumber: args.userInput.phoneNumber
+        });
+        const saveUser = await user.save();
+        return {
+          ...saveUser,
+          _id: saveUser.id,
+          name: saveUser.name,
+          email: saveUser.email,
+          password: null,
+          phoneNumber: saveUser.phoneNumber
+        };
+      }
+    } catch (err) {
+      throw err;
+    }
+  },
+  loginUser: async (args, req) => {
+    try {
+      // if (!req.isAuth) {
+      //   throw new Error("Unauthenticated user!");
+      // }
+      const user = await User.findOne({ email: args.email });
+      if (!user) {
+        throw new Error("User not exist!");
+      }
+      const isEqual = await bcrypt.compare(args.password, user.password);
+      if (!isEqual) {
+        throw new Error("Email or Password not correct!");
+      }
+      const token = jwt.sign(
+        { userId: user.id, email: user.email },
+        "supersecretkey",
+        { expiresIn: "1h" }
+      );
       return {
-        ...saveUser,
-        _id: saveUser.id,
-        name: saveUser.name,
-        email: saveUser.email,
-        password: null,
-        phoneNumber: saveUser.phoneNumber
+        userId: user.id,
+        token: token,
+        tokenExpiration: 1
       };
     } catch (err) {
       throw err;
     }
   }
-  //   loginUser: () => {}
 };
